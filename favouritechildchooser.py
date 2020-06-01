7416df12a92a7f86ee201442a0f0bb81b31d7253
anjolinea <62736574+anjolinea@users.noreply.github.com>
2020-05-31 22:49:43 -0400

    Update favouritechildchooser.py

--- favouritechildchooser.py
+++ favouritechildchooser.py
@@ -6,7 +6,7 @@ moms_favourite_child = random.choice(Mothers_Children)
 dads_favourite_child = random.choice(Fathers_Children)
 
 while True:
-    if moms_favourite_child == "joline" and dads_favourite_child == "joline":
+    if moms_favourite_child and dads_favourite_child == "joline":
         print("Mom's favourite child is", moms_favourite_child)
         print("Dad's favourite child is", dads_favourite_child)
         break
