743e39e844a5a344f198ef558d3498f1d57583c2
anjolinea <62736574+anjolinea@users.noreply.github.com>
2020-06-30 05:50:03 -0400

    Update assignmentweek2.py

--- applieddatascience4e/course1/assignmentweek2.py
+++ applieddatascience4e/course1/assignmentweek2.py
@@ -4,3 +4,10 @@ def answer_one():
     return best_country.index.tolist()[0]
 
 answer_one()
+
+# Question 2
+def answer_two():
+    the_answer = df[(df["Gold"]-df["Gold.1"]) == max(df["Gold"]-df["Gold.1"])]
+    return the_answer.index.tolist()[0]
+
+answer_two()
