96526ce06c388c6c2d8b9fb23ccaaf10696bf782
anjolinea <62736574+anjolinea@users.noreply.github.com>
2020-07-08 01:04:56 -0400

    Create assignment4.py

---
+++ applieddatascience4e/course1/assignment4.py
@@ -0,0 +1,34 @@
+# Question 1
+def get_list_of_university_towns():
+    df = pd.DataFrame(columns=["State", "RegionName"])
+    fhand = open("university_towns.txt")
+    num = 0
+    for line in fhand:
+        line = line.rstrip()
+        if "[edit]" in line:
+            line = line[:-6]
+            state = line
+            continue
+        else:
+            bracket_position = line.find("(")
+            regionname = line[:bracket_position-1]
+            df.loc[num] = state, regionname
+            num += 1
+    return df
+
+get_list_of_university_towns()
+
+# Question 2
+def get_recession_start():
+    df = pd.read_excel("gdplev.xls",skiprows=7)
+    df = df.rename(columns={"Unnamed: 6":"GDP", "Unnamed: 4": "Quarter"})
+    df = df[["Quarter", "GDP"]]
+    place_2000q1 = df[df["Quarter"] == "2000q1"].index.tolist()[0]
+    df = df[place_2000q1:]
+    for i in range(len(df)):
+        if df["GDP"].iloc[i] > df["GDP"].iloc[i+1] > df["GDP"].iloc[i+2]:
+            answer = (df["Quarter"].iloc[i+1])
+            break
+    return answer
+
+get_recession_start()
