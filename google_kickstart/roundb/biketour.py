903d82dfc08cb88e526839a0a030142ad4a10f6f
anjolinea <62736574+anjolinea@users.noreply.github.com>
2020-07-11 22:48:41 -0400

    Update biketour.py

--- google_kickstart/roundb/biketour.py
+++ google_kickstart/roundb/biketour.py
@@ -14,28 +14,28 @@ while T > 0:
     count += 1
     T -= 1
     
-# RunTime Error
+# Wrong Answer
 T = int(input())
-while T * 2 > 0:
+F = T+1
+while T > 0:
     line = input().split()
     line = list(map(int, line))
-    if (T * 2) % 2 == 1:
-        N, D = line
+    N, D = line
+    line = input().split()
+    line = list(map(int, line))
+    line.reverse()
+    # find starting point using last point
+    past_list = list(range(0, D + 1, line[0]))
+    start_point = past_list[-1]
+    if len(line) == 1:
+        print("Case #" + str(F-T) + ":", start_point)
     else:
-        line = line.split()
-        line = list(map(int, line))
-        line.reverse()
-        past_list = list(range(0, D + 1, line[0]))
-        start_point = past_list[-1]
-        if len(past_list) == 2:
-            print("Case #" + str(int(count / 2)) + ":", start_point)
-        else:
-            for num in line[1:]:
-                now_list = list(range(0, D + 1, num))
-                now_list.reverse()
-                for now_num in now_list:
-                    if now_num <= start_point:
-                        start_point = now_num
-                        break
-            print("Case #"+str(int(count/2))+":", now_num)
-    T -= 0.5
+        for num in line[1:]:
+            now_list = list(range(0, D + 1, num))
+            now_list.reverse()
+            for now_num in now_list:
+                if now_num <= start_point:
+                    start_point = now_num
+                    break
+        print("Case #"+str(F-T)+":", now_num)
+    T -= 1
