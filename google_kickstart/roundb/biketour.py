eadb951fd16be6fd7ed7e8abe0ae91db99fbee5b
anjolinea <62736574+anjolinea@users.noreply.github.com>
2020-07-12 00:13:29 -0400

    Update biketour.py

--- google_kickstart/roundb/biketour.py
+++ google_kickstart/roundb/biketour.py
@@ -14,28 +14,3 @@ while T > 0:
     count += 1
     T -= 1
     
-# Wrong Answer
-T = int(input())
-F = T+1
-while T > 0:
-    line = input().split()
-    line = list(map(int, line))
-    N, D = line
-    line = input().split()
-    line = list(map(int, line))
-    line.reverse()
-    # find starting point using last point
-    past_list = list(range(0, D + 1, line[0]))
-    start_point = past_list[-1]
-    if len(line) == 1:
-        print("Case #" + str(F-T) + ":", start_point)
-    else:
-        for num in line[1:]:
-            now_list = list(range(0, D + 1, num))
-            now_list.reverse()
-            for now_num in now_list:
-                if now_num <= start_point:
-                    start_point = now_num
-                    break
-        print("Case #"+str(F-T)+":", now_num)
-    T -= 1
